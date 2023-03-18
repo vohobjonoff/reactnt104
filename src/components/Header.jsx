7d2b7e1e6cfb8f5777bd231fe96aaab60247b298
kingbookk <kingbooktashkent@gmail.com>
2023-03-18 16:16:04 +0500

    new

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Hero from './Hero'
 import Nav from './Nav'
 
 export default function Header() {
   return (
       <div className='header'>
           <Nav />
+          <Hero />
     </div>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='hero'>Hero</div>
+  )
+}
